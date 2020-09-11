57838d5031a244a46c137161a55785cb842c9f57
Kyousuke17 <63723517+Kyousuke17@users.noreply.github.com>
2020-09-11 20:32:00 +0700

    Add files via upload

---
+++ dicodingweb/js/main.js
@@ -0,0 +1,19 @@
+const buttonCourse = document.getElementsByTagName('button');
+
+for(let i = 0;i < buttonCourse.length;i++){
+
+    buttonCourse[i].style.marginTop = '25px';
+    buttonCourse[i].style.boxSizing = 'border-box';
+    buttonCourse[i].style.backgroundColor = '#ff1900';
+    buttonCourse[i].style.color = '#ffffff';
+    buttonCourse[i].style.fontSize = '1rem';
+    buttonCourse[i].style.fontWeight = '400;'
+    buttonCourse[i].style.lineHeight = '1';
+    buttonCourse[i].style.padding = '10px';
+    buttonCourse[i].style.paddingLeft = '50px';
+    buttonCourse[i].style.paddingRight = '50px';
+    buttonCourse[i].style.border = '2px solid #ff1900';
+    buttonCourse[i].style.borderRadius = '5px';
+   
+}
+
